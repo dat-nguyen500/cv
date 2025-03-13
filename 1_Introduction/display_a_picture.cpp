6dc50250e9aee861dd63b45532bc1b50073594b5
dat-nguyen500 <dat.nguyen500@hcmut.edu.vn>
2025-03-13 16:34:14 +0700

    new file:   .vscode/c_cpp_properties.json new file:   1_Introduction/display_a_picture.cpp new file:   1_Introduction/lena.png new file:   1_Introduction/vc140.pdb modified:   CMakeLists.txt renamed:    build/.cmake/api/v1/reply/codemodel-v2-2b9b9b65f520510749d4.json -> build/.cmake/api/v1/reply/codemodel-v2-f48f24be395f9ffc310b.json renamed:    build/.cmake/api/v1/reply/index-2025-03-13T08-50-15-0501.json -> build/.cmake/api/v1/reply/index-2025-03-13T09-30-34-0250.json renamed:    build/.cmake/api/v1/reply/target-ALL_BUILD-RelWithDebInfo-964c73f89622e335e2b0.json -> build/.cmake/api/v1/reply/target-ALL_BUILD-Debug-1d5b81dd0b3775713510.json renamed:    build/.cmake/api/v1/reply/target-ALL_BUILD-Release-964c73f89622e335e2b0.json -> build/.cmake/api/v1/reply/target-ALL_BUILD-MinSizeRel-1d5b81dd0b3775713510.json renamed:    build/.cmake/api/v1/reply/target-ALL_BUILD-Debug-964c73f89622e335e2b0.json -> build/.cmake/api/v1/reply/target-ALL_BUILD-RelWithDebInfo-1d5b81dd0b3775713510.json renamed:    build/.cmake/api/v1/reply/target-ALL_BUILD-MinSizeRel-964c73f89622e335e2b0.json -> build/.cmake/api/v1/reply/target-ALL_BUILD-Release-1d5b81dd0b3775713510.json renamed:    build/.cmake/api/v1/reply/target-cv-Debug-8ca39d299ed0952d9c6c.json -> build/.cmake/api/v1/reply/target-cv-Debug-9ba0acb2844a87cfb4fb.json renamed:    build/.cmake/api/v1/reply/target-cv-MinSizeRel-96cc350b5c62c9b8ae01.json -> build/.cmake/api/v1/reply/target-cv-MinSizeRel-3eadd662384014e0ef99.json renamed:    build/.cmake/api/v1/reply/target-cv-RelWithDebInfo-1cb45370b40450ed2a4a.json -> build/.cmake/api/v1/reply/target-cv-RelWithDebInfo-f734fca9adfb271ce6fa.json renamed:    build/.cmake/api/v1/reply/target-cv-Release-ae0d4fadf8de8b2a8568.json -> build/.cmake/api/v1/reply/target-cv-Release-a983961fb476fc5bd67a.json modified:   build/ALL_BUILD.vcxproj modified:   build/Debug/cv.exe modified:   build/Debug/cv.pdb modified:   build/ZERO_CHECK.vcxproj modified:   build/cv.dir/Debug/cv.ilk modified:   build/cv.dir/Debug/cv.tlog/CL.command.1.tlog modified:   build/cv.dir/Debug/cv.tlog/CL.read.1.tlog modified:   build/cv.dir/Debug/cv.tlog/CL.write.1.tlog modified:   build/cv.dir/Debug/cv.tlog/Cl.items.tlog modified:   build/cv.dir/Debug/cv.tlog/link.command.1.tlog modified:   build/cv.dir/Debug/cv.tlog/link.read.1.tlog modified:   build/cv.dir/Debug/cv.tlog/link.secondary.1.tlog modified:   build/cv.dir/Debug/cv.tlog/link.write.1.tlog new file:   build/cv.dir/Debug/display_a_picture.obj deleted:    build/cv.dir/Debug/main.obj modified:   build/cv.dir/Debug/vc143.pdb modified:   build/cv.vcxproj modified:   build/cv.vcxproj.filters deleted:    main.cpp

---
+++ 1_Introduction/display_a_picture.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+
+int main() 
+{
+    cv::Mat img = cv::imread("1_introduction/lena.png");
+    if (img.empty()) 
+    {
+        std::cout << "Could not read the image: " << "1_introduction/lena.png" << std::endl;
+        return 1;
+    }
+    cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display window", img);
+    cv::waitKey(0);
+    return 0;
+}
